2b3589d7341307458a52c013545907d46525a6c3
Sagar <sagarpaul01@yahoo.com>
2025-12-06 12:14:24 +0600

    Added Sample file

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+      <View className="flex-1 justify-center items-center bg-black">
+        <Text className="text-lg text-blue-800 font-bold">Screen-এর মাঝখানে</Text>
+      </View>
+
   );
 }
